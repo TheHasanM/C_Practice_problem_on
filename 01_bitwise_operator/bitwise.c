5c00d9ac20efb77eb441db686ea5d99ef20367ec
TheHasanM <hasaniftikhareng@gmail.com>
2024-06-30 16:02:16 +0530

    print binary api added
    print binary api added

--- 01_bitwise_operator/bitwise.c
+++ 01_bitwise_operator/bitwise.c
@@ -3,13 +3,15 @@
 /*****************************************************************************************************************/
 /********************************************* Function declarations ********************************************/
 int re_set_bit(int, int, int);
+void print_binary(int);
 
 /*****************************************************************************************************************/
 
 int main()
 {
 
-    re_set_bit(5, 2, 0);
+    re_set_bit(3, 2, 1);
+    // print_binary(255);
 
     return 0;
 }
@@ -19,10 +21,12 @@ int main()
 /// @param n , given number
 /// @param p , given bit positon
 /// @param sr , to set or reset
-/// @return 
+/// @return
 /*---------------------------------------------------------------------------------------------------------------*/
 int re_set_bit(int n, int p, int sr)
 {
+    printf("Binary of Current No:");
+    print_binary(n); printf("\n");
     if (1 == sr) // check if set or reset is required
     {
         n = n | (1 << p); // to set the bit
@@ -31,8 +35,29 @@ int re_set_bit(int n, int p, int sr)
     {
         n = n & ~(1 << p); // to reset the bit
     }
-    printf(" new number: %d", n);
+    printf(" new number: %d \n", n);
+    printf("Binary of new No:");
+    print_binary(n); printf("\n");
     return 0;
 }
 
 /*---------------------------------------------------------------------------------------------------------------*/
+
+/// @brief Convert a given decimal number into its equivqlent binary
+/// @param num the given number
+/// @return ... 
+void print_binary(int num)
+{
+    int bin_arr[64] = {0};
+    int i,j=0;
+    while (num > 0)
+    {
+        bin_arr[i++] = num % 2;
+        num = num / 2;
+    }
+    for (j = i; j >= 0; j--)
+    {
+        printf("%d", bin_arr[j]);
+    }
+    // return 0;
+}
